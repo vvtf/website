c72d770b56f239495bcf8f927acc050dbb263b4d
Dominik Labuda <dominikla@gmail.com>
2019-04-28 17:53:47 +0200

    a

---
+++ data/mindmap/Lorem_Ipsum_Films.mm
@@ -0,0 +1,203 @@
+
+<map version="0.9.0">
+    <node TEXT="🎞 **Lorem Ipsum Films** 🎞" FOLDED="false" POSITION="right" ID="5baba34c5bb04bcb1323dd33" X_COGGLE_POSX="528" X_COGGLE_POSY="362">
+        <edge COLOR="#b4b4b4"/>
+        <font NAME="Helvetica" SIZE="17"/>
+        <node TEXT="Rebríček" FOLDED="false" POSITION="left" ID="cde1b75e4e43b09033190caf8303dffc">
+            <edge COLOR="#9ed56b"/>
+            <font NAME="Helvetica" SIZE="15"/>
+            <node TEXT="podľa filtru :red_flag:" FOLDED="false" POSITION="left" ID="abe1368c51d7921f2423ec2be98edf91">
+                <edge COLOR="#9cd266"/>
+                <font NAME="Helvetica" SIZE="15"/>
+                <node TEXT="dĺžka" FOLDED="false" POSITION="left" ID="b9d9f962349fe89a36cc8850cb5d3950">
+                    <edge COLOR="#a1d36b"/>
+                    <font NAME="Helvetica" SIZE="15"/>
+                </node>
+                <node TEXT="rok" FOLDED="false" POSITION="right" ID="ca1e0f212ffb8efd9f7264e09ea6902c">
+                    <edge COLOR="#9fd466"/>
+                    <font NAME="Helvetica" SIZE="15"/>
+                </node>
+                <node TEXT="kategória" FOLDED="false" POSITION="left" ID="dfc7188be3fa9ff166d109255323caf6">
+                    <edge COLOR="#a3d46e"/>
+                    <font NAME="Helvetica" SIZE="15"/>
+                </node>
+                <node TEXT="jazyk" FOLDED="false" POSITION="right" ID="1988d5ea29a01a887134459e7210b9e7">
+                    <edge COLOR="#9fd268"/>
+                    <font NAME="Helvetica" SIZE="15"/>
+                </node>
+                <node TEXT="režisér" FOLDED="false" POSITION="left" ID="b4eb0e66f8a41abe29f3e85a6d8784fa">
+                    <edge COLOR="#9cd064"/>
+                    <font NAME="Helvetica" SIZE="15"/>
+                </node>
+            </node>
+            <node TEXT="najlepšie filmy🥇" FOLDED="false" POSITION="right" ID="9930d700bebe013a8857265526594fbb">
+                <edge COLOR="#a4d573"/>
+                <font NAME="Helvetica" SIZE="15"/>
+            </node>
+            <node TEXT="najobľúbenejšie filmy:star:" FOLDED="false" POSITION="left" ID="7e1b422ec670b699e12248bfd12aef0a">
+                <edge COLOR="#a2d76d"/>
+                <font NAME="Helvetica" SIZE="15"/>
+            </node>
+        </node>
+        <node TEXT="Užívateľ" FOLDED="false" POSITION="left" ID="de9c6d12094b8d3068228ad31b5f0925">
+            <edge COLOR="#ebd95f"/>
+            <font NAME="Helvetica" SIZE="15"/>
+            <node TEXT="registrácia" FOLDED="false" POSITION="left" ID="067f4e1e87879210ef8c19c5ef264ef3">
+                <edge COLOR="#ead672"/>
+                <font NAME="Helvetica" SIZE="15"/>
+            </node>
+            <node TEXT="prihlásenie / odhlásenie" FOLDED="false" POSITION="left" ID="79a7afe128d8d51e5066ce79ee6f6050">
+                <edge COLOR="#ecd75e"/>
+                <font NAME="Helvetica" SIZE="15"/>
+            </node>
+            <node TEXT="vyhľadávanie" FOLDED="false" POSITION="left" ID="78f5899c0b06f3cc5da333612d645bbb">
+                <edge COLOR="#ecd862"/>
+                <font NAME="Helvetica" SIZE="15"/>
+            </node>
+            <node TEXT="Prihlásený užívateľ" FOLDED="false" POSITION="right" ID="f7482b6f4f941894aca54255ef28074a">
+                <edge COLOR="#efa670"/>
+                <font NAME="Helvetica" SIZE="15"/>
+                <node TEXT="odporúčania" FOLDED="false" POSITION="left" ID="bebcd091b88a5a52e975f777b07c2fa2">
+                    <edge COLOR="#f0a775"/>
+                    <font NAME="Helvetica" SIZE="15"/>
+                </node>
+                <node TEXT="zoznam obľúbených" FOLDED="false" POSITION="left" ID="12b3c15ad8314adc326fe62802787dda">
+                    <edge COLOR="#efa16c"/>
+                    <font NAME="Helvetica" SIZE="15"/>
+                </node>
+                <node TEXT="watch list" FOLDED="false" POSITION="left" ID="ec18ce0d1c05549bf24ce0e2581571d3">
+                    <edge COLOR="#f0a774"/>
+                    <font NAME="Helvetica" SIZE="15"/>
+                </node>
+                <node TEXT="komentáre" FOLDED="false" POSITION="left" ID="9079cec24cf7f87485ff2ac68cf0cc53">
+                    <edge COLOR="#eda472"/>
+                    <font NAME="Helvetica" SIZE="15"/>
+                </node>
+                <node TEXT="VIU" FOLDED="false" POSITION="right" ID="7bca72e30fd26cc1d5ee0221bc012b57">
+                    <edge COLOR="#e68782"/>
+                    <font NAME="Helvetica" SIZE="15"/>
+                    <node TEXT="pridávanie filmov" FOLDED="false" POSITION="right" ID="e0fbd8e4a9187ba02fe316c7e29b589a">
+                        <edge COLOR="#e57d79"/>
+                        <font NAME="Helvetica" SIZE="15"/>
+                    </node>
+                    <node TEXT="Admin" FOLDED="false" POSITION="right" ID="a2453ea0afb40c8d7fa8f9e6b9bdf9bc">
+                        <edge COLOR="#e096e9"/>
+                        <font NAME="Helvetica" SIZE="15"/>
+                        <node TEXT="pridávanie VIU" FOLDED="false" POSITION="right" ID="cb3e52f9d82f2fb8b0316012e75e5c96">
+                            <edge COLOR="#db8ce7"/>
+                            <font NAME="Helvetica" SIZE="15"/>
+                        </node>
+                        <node TEXT="schvaľovanie filmov" FOLDED="false" POSITION="right" ID="7cc035a5e7fe19659eab92e8c67473f7">
+                            <edge COLOR="#e09ee9"/>
+                            <font NAME="Helvetica" SIZE="15"/>
+                        </node>
+                        <node TEXT="správa systému" FOLDED="false" POSITION="right" ID="ba9985ca3bb811a7c5e3875d3a022110">
+                            <edge COLOR="#e09fea"/>
+                            <font NAME="Helvetica" SIZE="15"/>
+                        </node>
+                        <node TEXT="editácia údajov" FOLDED="false" POSITION="right" ID="0d2f62b553755579a99b09de3e561659">
+                            <edge COLOR="#e09bea"/>
+                            <font NAME="Helvetica" SIZE="15"/>
+                        </node>
+                    </node>
+                </node>
+                <node TEXT="hodnotenie filmu" FOLDED="false" POSITION="left" ID="bc97441f79ff35b425195de38dcb2b2a">
+                    <edge COLOR="#f0a571"/>
+                    <font NAME="Helvetica" SIZE="15"/>
+                </node>
+            </node>
+        </node>
+        <node TEXT="Film" FOLDED="false" POSITION="right" ID="3617a308c2d2b0f7431049ec3459d5cd">
+            <edge COLOR="#7aa3e5"/>
+            <font NAME="Helvetica" SIZE="13"/>
+            <node TEXT="tvorcovia" FOLDED="false" POSITION="right" ID="342f8a361f67586667a3a6e96e3280d0">
+                <edge COLOR="#80a9e7"/>
+                <font NAME="Helvetica" SIZE="12"/>
+                <node TEXT="zoznam filmov" FOLDED="false" POSITION="left" ID="41544aba9f9579887956f97fc5199ee0">
+                    <edge COLOR="#7ea9e5"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                </node>
+                <node TEXT="ocenenia" FOLDED="false" POSITION="left" ID="d945ad8d8faec848e3c591da35495ae0">
+                    <edge COLOR="#7da8e4"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                </node>
+                <node TEXT="biografia" FOLDED="false" POSITION="right" ID="7336579f95fa982b43254e94894e0486">
+                    <edge COLOR="#76a4e5"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                </node>
+            </node>
+            <node TEXT="herci" FOLDED="false" POSITION="right" ID="824350a6a9a1ac515c8f0cbbeec9fe22">
+                <edge COLOR="#82aae5"/>
+                <font NAME="Helvetica" SIZE="12"/>
+                <node TEXT="ocenenia" FOLDED="false" POSITION="right" ID="4131307af75b941c06cfa680ca0873e7">
+                    <edge COLOR="#85aee6"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                </node>
+                <node TEXT="zoznam filmov" FOLDED="false" POSITION="right" ID="b77127f1f59395b493a9c74f156c300b">
+                    <edge COLOR="#81abe6"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                </node>
+                <node TEXT="biografia" FOLDED="false" POSITION="right" ID="adc2f342ef306e49dc050955fa1b0181">
+                    <edge COLOR="#86afe7"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                </node>
+            </node>
+            <node TEXT="základné info" FOLDED="false" POSITION="right" ID="5f9bcbb0a477c82e271cb51b876e6084">
+                <edge COLOR="#7aa5e5"/>
+                <font NAME="Helvetica" SIZE="12"/>
+                <node TEXT="kategória" FOLDED="false" POSITION="right" ID="b4fba3f3068bcd3de05c77de4dce2d9f">
+                    <edge COLOR="#72a2e5"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                </node>
+                <node TEXT="dĺžka" FOLDED="false" POSITION="right" ID="db6ad2efa5d405158bf8fda61e278cec">
+                    <edge COLOR="#7faae6"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                </node>
+                <node TEXT="názov" FOLDED="false" POSITION="right" ID="52153fcc513457ab689693546593d912">
+                    <edge COLOR="#72a2e5"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                </node>
+                <node TEXT="krajina" FOLDED="false" POSITION="right" ID="d2d47ece7bbfbeb5200bf4db4c9cc5e8">
+                    <edge COLOR="#83ade8"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                </node>
+                <node TEXT="jazyk" FOLDED="false" POSITION="right" ID="55b7c8a0f4a720cde34bc580a0aab01f">
+                    <edge COLOR="#7aa8e7"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                </node>
+                <node TEXT="rok" FOLDED="false" POSITION="right" ID="2726082836ad819df12a75764b1152f0">
+                    <edge COLOR="#82abe5"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                </node>
+                <node TEXT="vekové obmedzenie" FOLDED="false" POSITION="right" ID="f534e258a4c8c4dc5f320299cd31ac29">
+                    <edge COLOR="#82abe5"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                </node>
+                <node TEXT="popis" FOLDED="false" POSITION="right" ID="8124194db3b52864dfccb9156ea777f3">
+                    <edge COLOR="#79a7e6"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                </node>
+            </node>
+            <node TEXT="ocenenia" FOLDED="false" POSITION="right" ID="7efc651abaec9868bf8b13861729dd86">
+                <edge COLOR="#79a5e6"/>
+                <font NAME="Helvetica" SIZE="12"/>
+            </node>
+            <node TEXT="hodnotenie" FOLDED="false" POSITION="right" ID="5eb275d80e0b5a15f9abef0539080214">
+                <edge COLOR="#75a1e2"/>
+                <font NAME="Helvetica" SIZE="12"/>
+            </node>
+            <node TEXT="komentáre" FOLDED="false" POSITION="right" ID="d629410ddec8087920b0ec39da894147">
+                <edge COLOR="#7ca7e7"/>
+                <font NAME="Helvetica" SIZE="12"/>
+            </node>
+            <node TEXT="foto" FOLDED="false" POSITION="left" ID="4aa09966027b0e8fe2736dc6b6e81939">
+                <edge COLOR="#76a2e3"/>
+                <font NAME="Helvetica" SIZE="12"/>
+            </node>
+            <node TEXT="trailer" FOLDED="false" POSITION="right" ID="a44266843ace59facb10724e9f610e22">
+                <edge COLOR="#82aae5"/>
+                <font NAME="Helvetica" SIZE="12"/>
+            </node>
+        </node>
+    </node>
+</map>
